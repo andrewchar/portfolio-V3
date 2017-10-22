270d4639a72f5e3b5b4f7cd52137eac6db0b5c4e
Andrew Charlebois <andrew.charlebois@mirumagency.com>
2017-10-21 21:38:47 -0400

    add some transitions to nav when scrolling past hero

--- resource/js/script.js
+++ resource/js/script.js
@@ -10,3 +10,30 @@ var waypoint = new Waypoint({
   },
   offset: 150
 })
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function stickyNav() {
+  var nav = document.getElementById('nav');
+  var intViewportHeight = window.innerHeight;
+  if (window.scrollY > 60 && (window.scrollY < intViewportHeight - 52)) {
+    nav.classList.add('hidden-nav');
+  } else {
+    nav.classList.remove('hidden-nav');
+  }
+}
+
+window.addEventListener("scroll", debounce(stickyNav));
